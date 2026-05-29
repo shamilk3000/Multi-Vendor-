79daf11f748d9882fde7635e996426dda324887c
unknown <shamilk3000@gmail.com>
2026-05-29 16:42:50 +0530

    git change 2

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ app.use("/api/webhook", webhookRoutes);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "Public")));
 app.use(cookieParser());
 // app.use(cors({
 //   origin: process.env.CORS_ORIGIN,
