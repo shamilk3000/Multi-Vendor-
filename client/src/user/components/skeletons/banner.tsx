2c9c0e1322cfc681a9e4d4290d5e3f541a75876c
unknown <shamilk3000@gmail.com>
2026-06-15 13:45:55 +0530

    banner

--- client/src/user/components/skeletons/banner.tsx
+++ client/src/user/components/skeletons/banner.tsx
@@ -1,3 +1,31 @@
+// import Skeleton from "react-loading-skeleton";
+// import "react-loading-skeleton/dist/skeleton.css";
+
+// function BannerSkeleton() {
+//   return (
+//     <div>
+//       {/* Heading */}
+//       <div className="text-center bg-black px-6 md:px-10 py-3">
+//         <div className="flex justify-center">
+//           <Skeleton width={280} height={35} />
+//         </div>
+//       </div>
+
+//       {/* Banner */}
+//       <div className="w-full bg-black">
+//         <Skeleton
+//           height={290}
+//           className="hidden md:block"
+//           baseColor="#1f1f1f"
+//           highlightColor="#2d2d2d"
+//         />
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default BannerSkeleton;
+
 import Skeleton from "react-loading-skeleton";
 import "react-loading-skeleton/dist/skeleton.css";
 
@@ -11,14 +39,14 @@ function BannerSkeleton() {
         </div>
       </div>
 
-      {/* Banner */}
-      <div className="w-full bg-black">
-        <Skeleton
-          height={290}
-          className="hidden md:block"
-          baseColor="#1f1f1f"
-          highlightColor="#2d2d2d"
-        />
+      {/* Mobile Banner */}
+      <div className="w-full bg-black md:hidden">
+        <Skeleton height={85} baseColor="#1f1f1f" highlightColor="#2d2d2d" />
+      </div>
+
+      {/* Desktop Banner */}
+      <div className="w-full bg-black hidden md:block">
+        <Skeleton height={290} baseColor="#1f1f1f" highlightColor="#2d2d2d" />
       </div>
     </div>
   );
